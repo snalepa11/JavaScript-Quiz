193a41c796f1911d2a7fe24a9462c7402e4201c5
Snalepa <snalepa1998@gmail.com>
2024-01-12 15:16:43 -0600

    Added questions to the quiz

---
+++ script.js
@@ -0,0 +1,69 @@
+
+
+
+var myQuestions = [
+    {
+        question: "What kinds of data types are the following: numbers, strings, booleans, and symbols?",
+        
+            answers: {
+                a: 'primary'
+                b: 'primitive'
+                c: 'singular'
+                d: 'major'
+            },
+
+            correctAnswer: 'b'
+        
+    }
+    {
+        question: "What are undeclared variables?",
+
+        answers: {
+        a: 'variables that do not exist in a program and are not declared'
+        b: 'variables that are written in the wrong syntax'
+        c: 'variables that return as false' 
+        d: 'variables that are too complex'
+        },
+
+        correctAnswer: 'a'
+
+    }
+    {
+        question: "What are undefined variables?",
+
+        answers: {
+            a: 'variables that return as false'
+            b: 'varaibles that are written in wrong syntax'
+            c: 'variables that have been declared but have not been given any value'
+            d: 'variables that do not exist in a program and are not declared'
+        },
+
+        correctAnswer: 'c'
+
+    }
+    {
+        question: "What 'this' keywor din javascript?",
+
+        answers: {
+            a: ' this, refers to the variable it is next to'
+            b: ' this, refers to a string saying this'
+            c: ' this, refers to an array'
+            d: ' this, refers to the object from where it was called'
+        },
+
+        correctAnswer: 'd'
+    }
+    {
+        question: "Which symbol is used for single line comments in javascript?",
+
+        answers: {
+            a: '//'
+            b: '/*'
+            c: '*'
+            d: '()'
+        },
+
+        correctAnswer: 'a'
+    }
+
+]
