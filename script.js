50c7566fdbf7b4ac0ec0bd8e8e42758fe941cba4
Snalepa <snalepa1998@gmail.com>
2024-05-06 14:28:47 -0400

    choice buttons

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 
 function runCode(){
 
-var timeEL = getElementById("timer")
+var timeEL = document.getElementById("timer")
+
+var introEL = document.getElementById("quiz-intro")
+
+var displayQuestionEl = document.getElementById("display-question")
+
+var displayChoicesEl = document.getElementById("display-choices")
 
 function sendMessage() {
     timeEL.textContent = ('Times up!');
@@ -14,12 +20,17 @@ function startTimer() {
             clearInterval(timerInterval);
             sendMessage();
         } else { 
-            timeEl.textContent='00:'+secondsLeft;
+            timeEL.textContent='00:'+secondsLeft;
         }
         secondsLeft--;
     }, 1000);
 }
 
+function hideIntro() {
+    introEL.classList.add("hide")
+}
+
+hideIntro();
 startTimer();
 
 
@@ -28,60 +39,64 @@ var myQuestions = [
         question: "What kinds of data types are the following: numbers, strings, booleans, and symbols?",
         
             answers: {
-                a: 'primary'
-                b: 'primitive'
-                c: 'singular'
+                a: 'primary',
+                b: 'primitive',
+                c: 'singular',
                 d: 'major'
             },
 
             correctAnswer: 'b'
         
-    }
+    },
+
     {
         question: "What are undeclared variables?",
 
         answers: {
-        a: 'variables that do not exist in a program and are not declared'
-        b: 'variables that are written in the wrong syntax'
-        c: 'variables that return as false' 
+        a: 'variables that do not exist in a program and are not declared',
+        b: 'variables that are written in the wrong syntax',
+        c: 'variables that return as false',
         d: 'variables that are too complex'
         },
 
         correctAnswer: 'a'
 
-    }
+    },
+
     {
         question: "What are undefined variables?",
 
         answers: {
-            a: 'variables that return as false'
-            b: 'varaibles that are written in wrong syntax'
-            c: 'variables that have been declared but have not been given any value'
+            a: 'variables that return as false',
+            b: 'varaibles that are written in wrong syntax',
+            c: 'variables that have been declared but have not been given any value',
             d: 'variables that do not exist in a program and are not declared'
         },
 
         correctAnswer: 'c'
 
-    }
+    },
+
     {
         question: "What 'this' keywor din javascript?",
 
         answers: {
-            a: ' this, refers to the variable it is next to'
-            b: ' this, refers to a string saying this'
-            c: ' this, refers to an array'
+            a: ' this, refers to the variable it is next to',
+            b: ' this, refers to a string saying this',
+            c: ' this, refers to an array',
             d: ' this, refers to the object from where it was called'
         },
 
         correctAnswer: 'd'
-    }
+    },
+
     {
         question: "Which symbol is used for single line comments in javascript?",
 
         answers: {
-            a: '//'
-            b: '/*'
-            c: '*'
+            a: '//',
+            b: '/*',
+            c: '*',
             d: '()'
         },
 
@@ -89,6 +104,43 @@ var myQuestions = [
     }
 
 ]
+
+function displayQuestion (){
+    let question = myQuestions[0].question
+    displayQuestionEl.textContent = question
 }
+
+function displayChoices (){
+    let choicea = myQuestions[0].answers.a
+    let choiceb = myQuestions[0].answers.b
+    let choicec = myQuestions[0].answers.c
+    let choiced = myQuestions[0].answers.d
+    
+    let choiceabutton = document.createElement("button")
+    choiceabutton.textContent = choicea
+
+    let choicebbutton = document.createElement("button")
+    choicebbutton.textContent = choiceb
+
+    let choicecbutton = document.createElement("button")
+    choicebbutton.textContent = choicec
+
+    let choicedbutton = document.createElement("button")
+    choicebbutton.textContent = choiced
+
+    displayChoicesEl.append(choiceabutton)
+    displayChoicesEl.append(choicebbutton)
+    displayChoicesEl.append(choicecbutton)
+    displayChoicesEl.append(choicedbutton)
+}
+
+displayChoices();
+
+var choicebtn
+
+displayQuestion();
+}
+
+
 
 document.getElementById('startBtn').addEventListener("click" , runCode);
