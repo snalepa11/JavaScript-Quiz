6a7bceeb07fc8ce01cd1164d6a0347acc93e5d5d
Snalepa <snalepa1998@gmail.com>
2024-05-19 16:15:53 -0400

    clean up and format script file

--- assets/script.js
+++ assets/script.js
@@ -1,170 +1,155 @@
 
-function runCode(){
+function runCode() {
 
-var timeEL = document.getElementById("timer")
+    var timeEL = document.getElementById("timer")
 
-var introEL = document.getElementById("quiz-intro")
+    var introEL = document.getElementById("quiz-intro")
 
-var displayQuestionEl = document.getElementById("display-question")
+    var displayQuestionEl = document.getElementById("display-question")
 
-var displayChoicesEl = document.getElementById("display-choices")
+    var displayChoicesEl = document.getElementById("display-choices")
 
-var topScoresEl = document.getElementById("scores-section")
+    var topScoresEl = document.getElementById("scores-section")
 
-function sendMessage() {
-    timeEL.textContent = ('Times up!');
-}
-
-function startTimer() {
-    var secondsLeft = 30;
-    var timerInterval = setInterval(function() {
-        if(secondsLeft === 0) {
-            clearInterval(timerInterval);
-            sendMessage();
-        } else { 
-            timeEL.textContent='00:'+secondsLeft;
-        }
-        secondsLeft--;
-    }, 1000);
-}
-
-function hideIntro() {
-    introEL.classList.add("hide")
-
-function hideScores()
-{
-    introEL.classList.add("hide")
-}
-}
-
-hideScores();
-hideIntro();
-startTimer();
-
-
-var myQuestions = [
-    {
-        question: "What kinds of data types are the following: numbers, strings, booleans, and symbols?",
-        
+    var myQuestions = [
+        {
+            question: "What kinds of data types are the following: numbers, strings, booleans, and symbols?",
             answers: {
                 a: 'primary',
                 b: 'primitive',
                 c: 'singular',
                 d: 'major'
             },
-
             correctAnswer: 'b'
-        
-    },
-
-    {
-        question: "What are undeclared variables?",
-
-        answers: {
-        a: 'variables that do not exist in a program and are not declared',
-        b: 'variables that are written in the wrong syntax',
-        c: 'variables that return as false',
-        d: 'variables that are too complex'
         },
-
-        correctAnswer: 'a'
-
-    },
-
-    {
-        question: "What are undefined variables?",
-
-        answers: {
-            a: 'variables that return as false',
-            b: 'varaibles that are written in wrong syntax',
-            c: 'variables that have been declared but have not been given any value',
-            d: 'variables that do not exist in a program and are not declared'
+        {
+            question: "What are undeclared variables?",
+            answers: {
+                a: 'variables that do not exist in a program and are not declared',
+                b: 'variables that are written in the wrong syntax',
+                c: 'variables that return as false',
+                d: 'variables that are too complex'
+            },
+            correctAnswer: 'a'
         },
-
-        correctAnswer: 'c'
-
-    },
-
-    {
-        question: "What 'this' keyword in javascript?",
-
-        answers: {
-            a: ' this, refers to the variable it is next to',
-            b: ' this, refers to a string saying this',
-            c: ' this, refers to an array',
-            d: ' this, refers to the object from where it was called'
+        {
+            question: "What are undefined variables?",
+            answers: {
+                a: 'variables that return as false',
+                b: 'varaibles that are written in wrong syntax',
+                c: 'variables that have been declared but have not been given any value',
+                d: 'variables that do not exist in a program and are not declared'
+            },
+            correctAnswer: 'c'
         },
-
-        correctAnswer: 'd'
-    },
-
-    {
-        question: "Which symbol is used for single line comments in javascript?",
-
-        answers: {
-            a: '//',
-            b: '/*',
-            c: '*',
-            d: '()'
+        {
+            question: "What 'this' keyword in javascript?",
+            answers: {
+                a: ' this, refers to the variable it is next to',
+                b: ' this, refers to a string saying this',
+                c: ' this, refers to an array',
+                d: ' this, refers to the object from where it was called'
+            },
+            correctAnswer: 'd'
         },
+        {
+            question: "Which symbol is used for single line comments in javascript?",
+            answers: {
+                a: '//',
+                b: '/*',
+                c: '*',
+                d: '()'
+            },
+            correctAnswer: 'a'
+        }
+    ]
 
-        correctAnswer: 'a'
+    function sendMessage() {
+        timeEL.textContent = ('Times up!');
     }
 
-]
+    function startTimer() {
+        var secondsLeft = 30;
+        var timerInterval = setInterval(function () {
+            if (secondsLeft === 0) {
+                clearInterval(timerInterval);
+                sendMessage();
+            } else {
+                timeEL.textContent = '00:' + secondsLeft;
+            }
+            secondsLeft--;
+        }, 1000);
+    }
 
-function displayQuestion (){
-    let question = myQuestions[0].question
-    displayQuestionEl.textContent = question
-}
+    function hideIntro() {
+        introEL.classList.add("hide")
+    }
 
-function displayChoices (){
-    let choicea = myQuestions[0].answers.a
-    let choiceb = myQuestions[0].answers.b
-    let choicec = myQuestions[0].answers.c
-    let choiced = myQuestions[0].answers.d
-    
-    let choiceabutton = document.createElement("button")
-    choiceabutton.textContent = choicea
+    // function hideScores() {
+    //     topScoresEL.classList.add("hide")
+    // }
 
-    let choicebbutton = document.createElement("button")
-    choicebbutton.textContent = choiceb
 
-    let choicecbutton = document.createElement("button")
-    choicebbutton.textContent = choicec
+    hideScores();
+    hideIntro();
+    startTimer();
 
-    let choicedbutton = document.createElement("button")
-    choicebbutton.textContent = choiced
 
-    displayChoicesEl.append(choiceabutton)
-    displayChoicesEl.append(choicebbutton)
-    displayChoicesEl.append(choicecbutton)
-    displayChoicesEl.append(choicedbutton)
+    function displayQuestion() {
+        let question = myQuestions[0].question
+        displayQuestionEl.textContent = question
+    }
+
+    function displayChoices() {
+        let choicea = myQuestions[0].answers.a
+        let choiceb = myQuestions[0].answers.b
+        let choicec = myQuestions[0].answers.c
+        let choiced = myQuestions[0].answers.d
+
+        let choiceabutton = document.createElement("button")
+        choiceabutton.textContent = choicea
+
+        let choicebbutton = document.createElement("button")
+        choicebbutton.textContent = choiceb
+
+        let choicecbutton = document.createElement("button")
+        choicebbutton.textContent = choicec
+
+        let choicedbutton = document.createElement("button")
+        choicebbutton.textContent = choiced
+
+        displayChoicesEl.append(choiceabutton)
+        displayChoicesEl.append(choicebbutton)
+        displayChoicesEl.append(choicecbutton)
+        displayChoicesEl.append(choicedbutton)
+    };
+
+    // function checkAnswer(answer){
+    // var answer = function answer(){
+    //     onclick()
+    // }
+    // if(){
+    //  choiceabutton | choiceabutton | choicecbutton | choicedbutton === correctAnswer
+    // }
+    // then
+
+    displayChoices();
+    displayQuestion();
+    checkAnswer();
 };
 
-function checkAnswer(answer){
-var answer = function answer(){
-    onclick()
-}
-if(){
- choiceabutton | choiceabutton | choicecbutton | choicedbutton === correctAnswer
-}
-then
-
-
-};
-
-function gameOver()
-
-function storeHighScores(event)
-
-function showHighScores() 
-
-displayChoices();
-displayQuestion();
-checkAnswer();
+function gameOver() {
+    if (secondsLeft === 0) {
+        // TODO
+    }
 }
 
+function storeHighScores(event) {
+    // TODO
+}
 
+function showHighScores() {
+    // TODO
+}
 
-document.getElementById('startBtn').addEventListener("click" , runCode);
+document.getElementById('startBtn').addEventListener("click", runCode);
